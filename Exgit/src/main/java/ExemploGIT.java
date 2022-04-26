302980ee6c39474707538b1278fe427c32385bd9
@RobsonB777 <robson.r.bn@hotmail.com>
2022-04-25 22:04:45 -0300

    criação de novo projeto

---
+++ Exgit/src/main/java/ExemploGIT.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author robson.gmoura
+ */
+public class ExemploGIT {
+    public static void main(String[] args) {
+        int soma = 1 + 1;
+        System.out.println("Resultado soma: + soma");
+        
+    }
+   
+}
